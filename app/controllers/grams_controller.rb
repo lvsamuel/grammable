6679f243bd1ca1db447eedf48616bf82da6b3db4
lvsam <lucillevsam@gmail.com>
2017-11-08 22:35:47 +0000

    picture uploader

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,7 +1,8 @@
 class GramsController < ApplicationController
-     before_action :authenticate_user!, only: [:new, :create]
+     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
      
     def index
+        @grams = Gram.all
     end
     
     def new
@@ -25,13 +26,14 @@ class GramsController < ApplicationController
     def edit
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
     end
     
     def update
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
         @gram.update_attributes(gram_params)
-
         if @gram.valid?
             redirect_to root_path
         else
@@ -42,16 +44,18 @@ class GramsController < ApplicationController
     def destroy
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
         @gram.destroy
         redirect_to root_path
     end
     
     private
     def gram_params
-       params.require(:gram).permit(:message)
+       params.require(:gram).permit(:message, :picture)
     end
     
-    def render_not_found
-        render plain: 'Not Found', status: :not_found
+    def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
     end
+
 end
