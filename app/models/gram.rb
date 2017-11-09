c76fef6e9d3177fc60bbbc68bd8f2e8a7da49e50
lvsam <lucillevsam@gmail.com>
2017-11-09 02:51:29 +0000

    Comments added

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,7 +1,9 @@
 class Gram < ApplicationRecord
     
     belongs_to :user
+    has_many :comments
     validates :message, presence: true
+    validates :picture, presence: true
     mount_uploader :picture, PictureUploader
     
 end
